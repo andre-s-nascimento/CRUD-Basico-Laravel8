07b3e67364db103eccc88c886801450a0ddd013f
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-04-05 23:33:30 -0300

    Ajustes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutosController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,7 +17,6 @@ use App\Http\Controllers\ProdutosController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::get('/produtos/novo', [ProdutosController::class, 'create']);
 Route::post('/produtos/novo', [ProdutosController::class, 'store'])->name('registrar_produto');
